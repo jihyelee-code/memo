196e75b374a3457af9933bbec89e1b61fe91ee14
jihyelee-code <sophischois@gmail.com>
2024-07-16 20:58:45 +0900

    update js output path

--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,7 @@ module.exports = {
     ],
     output: {
         path: path.join(__dirname, './docs'),
-        publicPath: "/",
+        publicPath: "./",
         filename: 'js/[name].bundle.js',
     },
     module: {
